36e24344a6e3b5f1ecf798a0b6a41b63be3b89b6
SHAKED NATALIE <shakednatalie@gmail.com>
2025-12-03 15:09:53 +0200

    add ProductQueryFilters interface for dynamic product filtering

---
+++ src/types/filters.ts
@@ -0,0 +1,30 @@
+import { ProductType, ConsumptionProtocol } from './product';
+
+export interface ProductQueryFilters {
+
+  name?: string;
+  type?: ProductType;
+  consumptionProtocol?: ConsumptionProtocol;
+
+  minZoom?: number;
+  minZoomGreater?: number;
+  minZoomGreaterEqual?: number;
+  minZoomLess?: number;
+  minZoomLessEqual?: number;
+
+  maxZoom?: number;
+  maxZoomGreater?: number;
+  maxZoomGreaterEqual?: number;
+  maxZoomLess?: number;
+  maxZoomLessEqual?: number;
+
+  resolutionBest?: number;
+  resolutionBestGreater?: number;
+  resolutionBestGreaterEqual?: number;
+  resolutionBestLess?: number;
+  resolutionBestLessEqual?: number;
+
+  boundingPolygonContains?: string;
+  boundingPolygonWithin?: string;
+  boundingPolygonIntersects?: string;
+}
