b237e2f9c04ed7f0185312140cca4f03f5d8da4a
SHAKED NATALIE <shakednatalie@gmail.com>
2025-12-09 11:39:17 +0200

    adding access to swagger via route

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import productRoutes from './routes/productRoutes';
 
+// Optional - swagger
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+import path from 'path';
+
 const app = express();
 
 app.use(express.json());
 
+// swagger
+const yamlPath = path.join(process.cwd(), 'openapi', 'openapi.yaml');
+const swaggerDocument = yaml.load(yamlPath);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// End of swagger
 
 export { app };
 app.use('/products', productRoutes);
